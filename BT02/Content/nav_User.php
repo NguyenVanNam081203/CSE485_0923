6e89f6dff950c1529c95adc9aa2f3015dc68d458
NguyenVanNam081203 <145735728+NguyenVanNam081203@users.noreply.github.com>
2023-10-03 23:17:22 +0700

    BT02

---
+++ BT02/Content/nav_User.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BTVN_Buoi_2_Hello_Thay</title>
+    <link rel="stylesheet" href="./bootstrap-5.2.3-dist/bootstrap-5.2.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./form.css?v= <?php echo time(); ?>">
+</head>
+<body>
+    <div class="d-flex pt-3">
+        <a class ="logo"  href=""><img src="./img/anh1.png" alt=""></a>
+        <a class ="logo" href=""><img  src="./img/anh.png" alt=""></a>
+    </div>
+    <ul class=" nav_control list-group list-group-flush">
+        <li class="list-group-iteam mt-4 pe-2 pt-3" ><a class="col text-black text-decoration-none d-inline-block fw-normal"  href=""><i class="bi bi-person-fill me-3" style="color: #ff6a59;;"></i>Users<span class="badge light badge-sm badge-primary ms-2 mb-0">Modules</span></a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-box-fill me-3" style="color: #ff6a59;;"></i>CMS<span class="badge light badge-sm badge-primary ms-2 mb-0">Modules</span></a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-gear me-3" style="color: #ff6a59;;"></i>Config<span class="badge light badge-sm badge-primary ms-2 mb-0">Modules</span></a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-grid me-3" style="color: #ff6a59;;"></i>Dashboard</a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-book me-3" style="color: #ff6a59;;"></i>Courses</a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-people me-3" style="color: #ff6a59;;"></i>Instructor</a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-info-circle me-3" style="color: #ff6a59;;"></i>Apps</a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-pie-chart me-3" style="color: #ff6a59;;"></i>Charts</a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-star me-3" style="color: #ff6a59;;"></i>Bootrap</a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-heart me-3" style="color: #ff6a59;;"></i>Plugins</a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-gear-wide me-3" style="color: #ff6a59;;"></i>Widget</a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-file-earmark-check me-3" style="color: #ff6a59;;"></i>Forms</a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-file-earmark-spreadsheet me-3" style="color: #ff6a59;;"></i>Table</a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+        <li class="list-group-iteam mt-4 pe-2" ><a class="col text-black text-decoration-none d-inline-block fw-normal" href=""><i class="bi bi-file-earmark-break-fill me-3" style="color: #ff6a59;;"></i>Pages</a><i class="bi bi-caret-right-fill text-secondary text-opacity-25 float-end "></i></li>
+    </ul>
+<script src="./bootstrap-5.2.3-dist/bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ BT02/user_selec.php
@@ -0,0 +1,5 @@
+<?php
+ include("connect.php");
+ $sql="select * from taikhoan";
+ $result=mysqli_query($connect,$sql); 
+?>
